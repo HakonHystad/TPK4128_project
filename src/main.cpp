7bf5bdbf4bce52108c48411ad9d1c769ee849d0f
HakonHystad <hakonhystad@gmail.com>
2017-04-02 23:50:35 +0200

    added body

--- src/main.cpp
+++ src/main.cpp
@@ -16,48 +16,143 @@ Section:                                          ~libs
 #include <string>
 
 #include "RFID/MFrec.h"
+#include "LCD/hdmiLCD.h"
 #include "network/standardsocket.h"
 #include "network/httpPostMaker.h"
 #include "network/serverClientCom.h"
+#include "keywords.h"
+
+/*#############################################################################################################
+
+Section:                                          ~forward declarations
+
+#############################################################################################################*/
+
+void sendSector( MFrec *rfid, int sector, HttpPostMaker *post, bool locked );
+
+/*#############################################################################################################
+
+Section:                                          ~main
+
+#############################################################################################################*/
 
 int main()
 {
+    /*-------------------------------------- setup LCD  ---------------------------------------*/
+
+    HdmiLCD LCD( 0x27 );
+    LCD.init(); 
+    
     /*-------------------------------------- setup network COM ---------------------------------------*/
 
     std::string serverName = "folk.ntnu.no";
     std::string domain = "/haakonhy/php/requestHandling.php";
     
-    StandardSocket *p_socketToServer = new StandardSocket( &serverName, 80, STREAM );
+    StandardSocket socketToServer( &serverName, 80, STREAM );
     
-    p_socketToServer->connectToServer();// blocking
+    socketToServer.connectToServer();// blocking
 
-    int clientSocketId = p_socketToServer->getSocketIdentity();
+    int clientSocketId = socketToServer.getSocketIdentity();
 
     if( clientSocketId <0 )
     {
-	std::cout << "no clientSocket\n";
-	return 1;
+	std::cerr << "no clientSocket\n";
+	LCD.print( "Failed connection" );
+	return -1;
     }
 
-    ServerClientCom COM = ServerClientCom( clientSocketId );// handles sending receiving
+    HttpPostMaker REQ = HttpPostMaker( clientSocketId, domain ); 
 
-    HttpPostMaker REQ = HttpPostMaker( domain ); 
-
-
-   
     /*-------------------------------------- try default keys  ---------------------------------------*/
 
     MFrec RFID;
 
     RFID.initCom();// blocking
 
+
     int lockedSectors[16];
-    int nrOflockedSectors = 0;
-    
-    for( int i = 0; i<16; i++)
+    int nrOfLockedSectors = 0;
+    int unlockedSector = 0;
+
+    for( int sector = 0; sector<16; sector++)
     {
+	
+	LCD.print( "Sector " + std::to_string( sector ) );
+
+	if( !RFID.authenticateOnChip( AUTHENT_A, sector*4 ) )// takes blockAddr, not sector..
+	{
+	    lockedSectors[ nrOfLockedSectors++ ] = sector;
+	    sendSector( &RFID, sector, &REQ, true );
+
+	}
+	else
+	{
+	    unlockedSector = sector;
+	    sendSector( &RFID, sector, &REQ, false );
+	}
+
+    }// for each sector
+
+    if( nrOfLockedSectors>15 )
+    {
+	LCD.print( "No default" );
+	std::cout << "No sectors use a default key\n";
+	return 0;
     }
+
+    
 
     return 0;
 
 }
+
+
+/*=============================================================================================================
+
+NAME:                                             ~sendSector
+ARGUMENT(S):
+DESCRIPTION: read a sector from RFID card and sends it to server
+RETURN:
+
+==============================================================================================================*/
+void sendSector( MFrec *rfid, int sector, HttpPostMaker *post, bool locked )
+{
+    unsigned char block[18]; 
+
+    for( int blk = 0; blk<4; blk++ )
+    {
+	// reset block
+	for( int i = 0; i<16; i++) block[i] = 'X';
+
+	if( !locked )
+	    rfid->readBlock( (sector*4) + blk, block, 18 );// 16 data bytes + 2 checksum bytes
+
+	/* POST variables are hidden since this is public */
+	post->addToBody( var1 + "=" + std::to_string( (sector*4)+blk ) );
+
+	
+	// convert to ascii if possible
+	std::string asciiRep="";
+	    
+	for( int i = 0; i<16; i++ )
+	{
+	    post->addToBody( var2 + std::to_string(i) + "=" + std::to_string( (int)block[i] ) );
+        
+	    if( block[i] >=32 && block[i]<127 )// readable char
+	    {
+		asciiRep += std::to_string( (int)block[i] );// assuring no truncation with to_string
+	    }
+	    else
+	    {
+		asciiRep += ".";
+	    }
+	}
+
+	post->addToBody( var3 + "=" + asciiRep );
+    }// for each block
+
+    post->send();
+}
+
+
+    
